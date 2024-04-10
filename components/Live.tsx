90bcc4bf6c4f6cecfa272e93778b29043082cfae
RuchikaKadam <kadamruchikafullstack@gmail.com>
2024-04-10 17:07:34 +0530

    live chat implementation added

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,10 +1,16 @@
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import LiveCursors from "./cursor/LiveCursors";
 import { useMyPresence, useOthers } from "@/liveblocks.config";
+import CursorChat from "./cursor/CursorChat";
+import { CursorMode } from "@/types/type";
 
 const Live = () => {
   const others = useOthers();
   const [{cursor},  updateMyPresence] = useMyPresence() as any;
+
+  const [cursorState, setCursorState] = useState({
+    mode: CursorMode.Hidden,
+  })
 
   const handlePointerMove = useCallback((event: React.PointerEvent) => {
     event.preventDefault();
@@ -16,7 +22,7 @@ const Live = () => {
   },[])
 
   const handlePointerLeave = useCallback((event: React.PointerEvent) => {
-    event.preventDefault();
+    setCursorState({mode:CursorMode.Hidden})
 
     updateMyPresence({cursor:null, message: null});
   },[])
@@ -37,6 +43,16 @@ const Live = () => {
 
       className="h-[100vh] w-full flex justify-center items-center text-center"
     >
+      <h1>Live Page</h1>
+
+      {cursor && (
+        <CursorChat 
+        cursor={cursor} 
+        cursorState= {cursorState}
+        setCursorState ={setCursorState}
+        updateMyPresence= {updateMyPresence}
+        />
+      )}
       <LiveCursors others = {others}/>
     </div>
   )
--- components/cursor/CursorChat.tsx
+++ components/cursor/CursorChat.tsx
@@ -1,8 +1,41 @@
+import CursorSVG from '@/public/assets/CursorSVG'
+import { CursorChatProps, CursorMode } from '@/types/type'
 import React from 'react'
 
-const CursorChat = () => {
+const CursorChat = ({
+  cursor, 
+  cursorState,
+  setCursorState, 
+  updateMyPresence
+}: CursorChatProps
+) => {
   return (
-    <div>CursorChat</div>
+    <div
+    className='absolute top-0 left-0'
+    style={{
+      transform: `translatex(${cursor.x}px)`
+      transform: `translatey(${cursor.y}px)`
+    }}
+    >
+
+      {cursorState.mode === CursorMode.Chat && (
+        <>
+          <CursorSVG color='#000' />
+
+          <div className='absolute top-5 left-2 bg-blue-400 px-4 py-2 text-sm leading-relaxed text-white rounded-[20px]'>
+            {cursorState.previousMessage && (
+              <div>
+                {cursorState.previousMessage}
+              </div>
+            )}
+
+            <input className='z-10'/>
+
+          </div>
+        </>
+      )}
+
+    </div>
   )
 }
 
