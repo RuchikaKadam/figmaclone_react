4777ee2df5db98a7c7aaeb4da70877f96b5de031
RuchikaKadam <kadamruchikafullstack@gmail.com>
2024-04-10 16:33:16 +0530

    Live cursor completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import { Room } from "./Room";
-// import { CollaborativeApp } from "./CollaborativeApp";
+"use client";
+
+import Live from "@/components/Live";
 
 export default function Page() {
   return ( 
-    <div className="h-[100vh] w-full flex justify-center items-center text-center">
+    <div>
       <h1 className="text-xl bg-black text-white">REACT</h1>
-
+      <Live />
     </div>
     
   );
