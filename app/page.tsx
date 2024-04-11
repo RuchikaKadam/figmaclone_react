a687ecb0568ac915a454c903c3664ab43c971c6a
RuchikaKadam <kadamruchikafullstack@gmail.com>
2024-04-11 17:43:35 +0530

    sidebars created

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
 "use client";
 
+import LeftSideBar from "@/components/LeftSideBar";
 import Live from "@/components/Live";
 import Navbar from "@/components/Navbar";
+import RightSideBar from "@/components/RightSideBar";
 
 export default function Page() {
   return ( 
     <main className="h-screen overflow-hidden">
       <Navbar />
       <section className="flex h-full flex-row"></section>
+      <LeftSideBar />
       <Live />
+      <RightSideBar />
     </main>
     
   );
